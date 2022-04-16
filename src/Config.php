f178ce73dc6557c2ebe98d2542f6788319b8e178
Ivanildo Cândido <systechtecnologia@gmail.com>
2022-04-15 21:08:55 -0300

    Add controllers and initial classes

--- src/Config.php
+++ src/Config.php
@@ -2,13 +2,13 @@
 namespace src;
 
 class Config {
-    const BASE_DIR = '/mvc/public';
+    const BASE_DIR = '/virtual_life/public';
 
     const DB_DRIVER = 'mysql';
-    const DB_HOST = 'localhost';
-    const DB_DATABASE = 'test';
+    const DB_HOST = '144.22.192.13';
+    const DB_DATABASE = 'virtual_life';
     CONST DB_USER = 'root';
-    const DB_PASS = '';
+    const DB_PASS = 'ag72qjh5';
 
     const ERROR_CONTROLLER = 'ErrorController';
     const DEFAULT_ACTION = 'index';
--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -1,20 +1,20 @@
 <?php
+
 namespace src\controllers;
 
 use \core\Controller;
 
-class HomeController extends Controller {
+class HomeController extends Controller
+{
+    private $loggedUser;
 
-    public function index() {
+    public function __construct()
+    {
+        $this->redirect('/login');
+    }
+
+    public function index()
+    {
         $this->render('home', ['nome' => 'Systech Tecnologia']);
     }
-
-    public function sobre() {
-        $this->render('sobre');
-    }
-
-    public function sobreP($args) {
-        print_r($args);
-    }
-
 }
