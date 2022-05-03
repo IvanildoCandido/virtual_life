a7ab4c9fc4ec2a03fe4e64bffb5f72a0bc27423b
Ivanildo Cândido <systechtecnologia@gmail.com>
2022-05-03 07:45:20 -0300

    Like ajax function

--- src/views/partials/footer.php
+++ src/views/partials/footer.php
@@ -7,5 +7,8 @@
 </body>
 
 </html>
+<script type="text/javascript">
+    const BASE = '<?= $base; ?>';
+</script>
 <script type="text/javascript" src="<?= $base ?>/assets/js/script.js"></script>
 <script type="text/javascript" src="<?= $base ?>/assets/js/vanillaModal.js"></script>
